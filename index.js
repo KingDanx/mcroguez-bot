fa6bd8df0969b2c641dedb210b92382a7a43597d
Dan Prudhomme <dannyprud@yahoo.com>
2025-09-02 17:11:02 -0500

    add new endpoint to try to get the messages to a listenable state by the time they hit the user

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { Client, GatewayIntentBits, AttachmentBuilder } from "discord.js";
 import { randomUUID } from "crypto";
-import { config } from "dotenv";
+// import { config } from "dotenv";
 
-config();
+// config();
 
 const client = new Client({
   intents: [
@@ -19,13 +19,17 @@ function fixHisGramer(message) {
     .replaceAll("\r", "")
     .replaceAll("\n", "")
     .replaceAll(" im ", " I'm ")
+    .replaceAll(" idk ", " i don't know ")
     .replaceAll(" ill ", " I'll ")
     .replaceAll(" ur ", " your ")
     .replaceAll(" dont ", " don't ")
     .replaceAll(" didnt ", " didn't ")
     .replaceAll(" wont ", " won't ")
     .replaceAll(" assa ", " assassination ")
-    .replaceAll(" def ", " definitely ");
+    .replaceAll(" def ", " definitely ")
+    .replaceAll(" st ", " secret technique ")
+    .replaceAll(" sd ", " shadow dance ")
+    .replaceAll(" ss ", " shadow step ");
 }
 
 client.once("clientReady", () => {
@@ -45,9 +49,33 @@ client.on("messageCreate", async (message) => {
 
   if (message.content.length >= Number(process.env.CHARACTER_LIMIT)) {
     try {
+      const aiReqBody = {
+        model: "deepseek-r1:8b",
+        think: false,
+        stream: false,
+        system:
+          'you are a simple ai tasked with processing user text to fix grammar and expand abbreviations without changing the original words, including profanities, unless absolutely necessary for clarity.  Remove the abbreviations completely, do not include them after words in parentheses. Remove the abbreviations completely, do not include them after words in parentheses. Remove the abbreviations completely, do not include them after words in parentheses. You are not to include anything in your output that is not the output of what I requested. Do not Tell me what you corrected. You are to respond ONLY in raw JSON format with the an output "message" key containing the modified input text with perfect grammar and a "summery" key with a one sentence summary of the input prompt in world of warcraft style gamer banter. Do not put the RAW JSON in mark down syntax block',
+        prompt: fixHisGramer(message.content),
+      };
+
+      const aiRes = await fetch(process.env.AI_ENDPOINT, {
+        method: "POST",
+        body: JSON.stringify(aiReqBody),
+      });
+
+      if (!aiRes.ok) {
+        throw new Error("failed to get audio from speech endpoint.");
+      }
+
+      const data = await aiRes.json();
+
+      const aiResponse = JSON.parse(
+        data.response.replaceAll("```json", "").replaceAll("```", "")
+      );
+
       const reqBody = {
         download_format: "mp3",
-        input: fixHisGramer(message.content),
+        input: aiResponse.message,
         return_download: true,
         speed: 1,
         stream: false,
@@ -67,7 +95,10 @@ client.on("messageCreate", async (message) => {
 
       const file = Buffer.from(blob);
 
+      console.log(aiResponse);
+
       await message.reply({
+        content: aiResponse.summary,
         files: [
           new AttachmentBuilder(file, {
             name: randomUUID() + ".mp3",
